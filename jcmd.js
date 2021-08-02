dc7fc19c5a67cf57e2a682e6bf1a577a9e5594c1
DiamondDemon669 <62653580+DiamondDemon669@users.noreply.github.com>
2021-08-03 00:52:16 +0100

    interpreter is now an extension of jcmd class and inherits all properties

--- jcmd.js
+++ jcmd.js
@@ -1,19 +1,16 @@
 class jcmd {
 	constructor(consoleid, height="200px", width="50%", clear=["P", "STRONG", "H1", "H2", "UNDEFINED"]) {
 		this.console = document.getElementById(consoleid);
-		this.console.style = "height: {height}; width: {width}; background-color: #0C0C0C".replace("{height}", height).replace("{width}", width);
 		var clearlist = {};
 		clear.forEach(function(value, index, array) { clearlist[value] = value; });
 		this.clearlist = clearlist;
 		var labelnode = document.createElement("label");
 		labelnode.id = "JCMDlabel";
 		labelnode.innerHTML = "> "
-		labelnode.style = "color: #CCCCCC";
 		var inputnode = document.createElement("input");
 		inputnode.id = "JCMDinput";
-		inputnode.style = "background-color: #0C0C0C; color: #CCCCCC"
 		var stylenode = document.createElement("style");
-		stylenode.innerHTML = "#JCMDinput:focus { outline: 0px; }";
+		stylenode.innerHTML = "#JCMDinput:focus { outline: 0px; } #JCMDinput { background-color: #0C0C0C; color: #CCCCCC; } #JCMDlabel { color: #CCCCCC; } #{console} { height: {height}; width: {width}; background-color: #0C0C0C; } .output { color: #CCCCCC; margin-block: 0; }".replace("{console}", consoleid).replace("{height}", height).replace("{width}", width);
 		this.console.appendChild(labelnode);
 		this.console.appendChild(inputnode);
 		this.console.appendChild(stylenode);
@@ -26,7 +23,7 @@ class jcmd {
 	output(inputtext, inputtype="p") {
 		var output = document.createElement(inputtype);
 		output.innerHTML = inputtext;
-		output.style = "color: #CCCCCC; margin-block: 0";
+		output.classList.add("output");
 		var br = document.createElement("br");
 		this.console.insertBefore(br, this.prompter);
 		this.console.insertBefore(output, br);
@@ -41,11 +38,13 @@ class jcmd {
 		this.prompter.innerHTML = newval;
 	}
 }
-class interpreter {
-	constructor(console) {
-		this.jcmd = console;
+class interpreter extends jcmd {
+	constructor(consoleid, height="200px", width="50%", clear=["P", "STRONG", "H1", "H2", "UNDEFINED"]) {
+		super(consoleid, height, width, clear)
 		this.binder = {};
+		this.fdata = {};
 		this.binder["$all"] = function() { return null; };
+		super.onInput(this.activate.bind(this));
 	}
 	bind(command, callback) {
 		this.binder[command] = callback;
@@ -53,13 +52,13 @@ class interpreter {
 	unBind(command) {
 		this.binder[command] = undefined;
 	}
-	activate(event) {
-		if (this.jcmd.input.value in this.binder) {
-			this.binder[this.jcmd.input.value]();
+	activate() {
+		if (this.input.value in this.binder) {
+			this.binder[this.input.value]();
 		} else {
 			for (var x in this.binder) {
 				var found = false;
-				if (x.includes("$else") && this.jcmd.input.value.includes(x.replace("$else", ""))) {
+				if (x.includes("$else") && this.input.value.includes(x.replace("$else", ""))) {
 					this.binder[x]();
 					found = true;
 					break
